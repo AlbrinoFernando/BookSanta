dfe1ca54f59291eb499f74f5ecd4b2abf1ba19e3
The99thMage <foxycodedojo@gmail.com>
2021-05-10 14:29:46 -0700

    Google Login

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -5,6 +5,7 @@ import {View, Text, TouchableOpacity,
 import db from "../config"
 import SantaClaus from "../components/SantaClaus"
 import firebase from "firebase";
+import * as Google from 'expo-google-app-auth';
 
 export default class WelcomeScreen extends React.Component{
   constructor(){
@@ -20,11 +21,78 @@ export default class WelcomeScreen extends React.Component{
     }
   }
 
-  signUpWithGoogle =()=>{
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
+  onSignIn = (googleUser) => {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!this.isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken
+        );
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential).then(function(){
+          db.collection("users").add({
+            email_id: result.user.email,
+            first_name: result.user.given_name,
+            last_name: result.user.family_name,
+            address: "",
+            contact: "",
+            isBookRequestActive: false
+          })
+          this.props.navigation.navigate("donateBooks")
+        }).catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    }).bind(this);
+  }
+  
+   isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        //androidClientId: YOUR_CLIENT_ID_HERE,
+        behavior: "web",
+        iosClientId: "332580148220-8cvj3c39dlfob2r49tdeevtrthr5vben.apps.googleusercontent.com",
+        scopes: ['profile', 'email'],
+      });
+  
+      if (result.type === 'success') {
+        this.onSignIn(result)
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
   userSignUp=(email, password, confirmPassword)=>{
     if(password !== confirmPassword){
       return Alert.alert("Your passwords don't match!")
@@ -204,7 +272,7 @@ export default class WelcomeScreen extends React.Component{
 
             <TouchableOpacity style={styles.signInButton}
             onPress={()=>{
-                this.signUpWithGoogle();
+                this.signInWithGoogleAsync();
             }}>
             <Text style={styles.signInButtonText}>Sign In w/ Google</Text>
             </TouchableOpacity>
